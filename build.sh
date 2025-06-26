d2e63b3d57b694b9bf0eabd992372edaad036b9e
Your Correct GitHub Username <praoabhay@gmail.com>
2025-06-26 15:04:00 +0530

    changes made

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
-apt-get update && apt-get install -y git-lfs
+
+# Install git-lfs in Render-compatible way
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
+apt-get install -y git-lfs
+
+# Enable and pull LFS files
 git lfs install
 git lfs pull
 
+# Install Python dependencies
 pip install -r requirements.txt
